4fac42b5abb265e9280e4b0399f8b4a6debb1d88
VladislavkiSelov <vladislavkiselev.2007@gmail.com>
2022-12-31 19:17:47 +0200

    commit1

--- Homework_4/script.js
+++ Homework_4/script.js
@@ -1,9 +1,22 @@
-function sum(x) {
-    return function (y){
-    return x + y
+function sum(){
+let res = 0;
+return function (y){
+    res += y;  return  res;
 }
 }
-let add = sum(0)
+
+let add = sum();
 console.log(add(3))
 console.log(add(5))
 console.log(add(20))
+
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+function removeElement(array){
+  return array.splice(4,1)
+}
+removeElement(array)
+console.log(array)
+
+
+
