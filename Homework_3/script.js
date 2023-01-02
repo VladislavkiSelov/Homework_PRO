6eae2ff66d0e6af33b970e40fc5abeb2870f14ac
VladislavkiSelov <vladislavkiselev.2007@gmail.com>
2023-01-02 11:48:04 +0200

    11

--- Homework_3/script.js
+++ Homework_3/script.js
@@ -1,35 +1,14 @@
 // Задание №1
 const amount = 4;
 const valueToFill = 'hello';
-function fillArray(amount, valueToFill){
-let add=[]
-for(let i = 0;i<amount;i++){
+function fillArray(amount, valueToFill) {
+  let add = []
+  for (let i = 0; i < amount; i++) {
     add.push(valueToFill)
-}
-return add
+  }
+  return add
 }
 console.log(fillArray(amount, valueToFill))
-
-
-// // // Задание №2 (НЕ ПОНИМАЮ ЧТО Я ДЕЛАЮ НЕ ТАК, ПРОГРАММА НЕ РАБОТАЕТ, МОЖНО РЕШЕНИЕ?)
-// let obj = {
-//   width: 10, 
-//   height: 5,
-//   title: 'Test Array',
-//   count: 4
-// }
-
-
-// function multiplyNumeric(){
-//   let values = Object.values(obj)
-//   values.forEach(function(item){
-//     if (typeof item  === 'number'){'number'**2}})
-//   return  values
-// }
-
-//  multiplyNumeric(obj)
-//   console.log(obj)
-
 
 
 // // Задание №2 (НЕ ПОНИМАЮ ЧТО Я ДЕЛАЮ НЕ ТАК, ПРОГРАММА НЕ РАБОТАЕТ, МОЖНО РЕШЕНИЕ?)
@@ -42,20 +21,42 @@ let obj = {
 
 
 function multiplyNumeric(){
-  let obj_final={}
-  let keys = Object.keys(obj)
-  for(let key = 0;key<keys.length; key++){
-  
- }
   let values = Object.values(obj)
-  for(let key = 0;key<values.length; key++){
-    
- }
-
- console.log(obj_final)
- return obj_final
+  values.forEach(function(item){
+    if (typeof item  === 'number'){'number'**2}})
+  return  values
 }
+
  multiplyNumeric(obj)
   console.log(obj)
+
+
+
+// // // Задание №2 (НЕ ПОНИМАЮ ЧТО Я ДЕЛАЮ НЕ ТАК, ПРОГРАММА НЕ РАБОТАЕТ, МОЖНО РЕШЕНИЕ?)
+// let obj = {
+//   width: 10,
+//   height: 5,
+//   title: 'Test Array',
+//   count: 4
+// }
+// function multiplyNumeric() {
+//   let obj_final = {keys, values}
+//   let keys = Object.keys(obj)
+//   for (let key = 0; key < keys.length; key++) {
+//   }
+//   let values = Object.values(obj)
+//   for (let i = 0; i < values.length; i++) {
+//     if (typeof i === 'number') { 'number' ** 2 }
+//   }
+//   return obj_final
+// }
+// multiplyNumeric(obj)
+// console.log(obj)
+
+
+
+
+
+
 
 
