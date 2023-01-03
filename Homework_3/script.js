b12c66118868dab7f51d11249c6a99c03ffe262c
VladislavkiSelov <vladislavkiselev.2007@gmail.com>
2023-01-03 10:38:26 +0200

    commit 14

--- Homework_3/script.js
+++ Homework_3/script.js
@@ -11,24 +11,113 @@ return[add]
 console.log(fillArray(amount, valueToFill))
 
 
-/ // Задание №2 (НЕ ПОНИМАЮ ЧТО Я ДЕЛАЮ НЕ ТАК, ПРОГРАММА НЕ РАБОТАЕТ, МОЖНО РЕШЕНИЕ?)
+
+
+
 let obj = {
-  width: 10, 
+  width: 10,
   height: 5,
   title: 'Test Array',
   count: 4
 }
 
 
-function multiplyNumeric(){
-  let values = Object.values(obj)
-  values.forEach(function(item){
-    if (typeof item  === 'number'){'number'**2}})
-  return  values
+function multiplyNumeric() {
+  for (let yyy in obj) {
+    if (typeof obj[yyy] === 'number') { obj[yyy] ** 2 }
+  }
 }
+multiplyNumeric(obj)
+console.log(obj)
 
- multiplyNumeric(obj)
-  console.log(obj)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// / // Задание №2 (НЕ ПОНИМАЮ ЧТО Я ДЕЛАЮ НЕ ТАК, ПРОГРАММА НЕ РАБОТАЕТ, МОЖНО РЕШЕНИЕ?)
+// let obj = {
+//   width: 10, 
+//   height: 5,
+//   title: 'Test Array',
+//   count: 4
+// }
+
+
+// function multiplyNumeric(){
+//   let values = Object.values(obj)
+//   values.forEach(function(item){
+//     if (typeof item  === 'number'){'number'**2}})
+//   return  values
+// }
+
+//  multiplyNumeric(obj)
+//   console.log(obj)
 
 
 
@@ -52,5 +141,25 @@ function multiplyNumeric(){
 // }
 // multiplyNumeric(obj)
 // console.log(obj)
+
+
+// // Задание №2 (НЕ ПОНИМАЮ ЧТО Я ДЕЛАЮ НЕ ТАК, ПРОГРАММА НЕ РАБОТАЕТ, МОЖНО РЕШЕНИЕ?)
+// let obj = {
+//   width: 10,
+//   height: 5,
+//   title: 'Test Array',
+//   count: 4
+// };
+
+
+// for(let yyy in obj){
+//   if (typeof'number' == obj[yyy]) {obj[yyy] * obj[yyy] }
+// }
+
+// console.log(obj)
+
+
+
+// Задание №2 (НЕ ПОНИМАЮ ЧТО Я ДЕЛАЮ НЕ ТАК, ПРОГРАММА НЕ РАБОТАЕТ, МОЖНО РЕШЕНИЕ?)
 
 
