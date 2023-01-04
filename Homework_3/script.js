b9f3c6554b96cf635dd214039b837ddd2640b6a0
VladislavkiSelov <vladislavkiselev.2007@gmail.com>
2023-01-04 20:10:02 +0200

    FINAL

--- Homework_3/script.js
+++ Homework_3/script.js
@@ -1,16 +1,14 @@
 // Задание №1
 const amount = 4;
 const valueToFill = 'hello';
-function fillArray(amount, valueToFill){
-let add=[]
-for(let i = 0;i<amount;i++){
+function fillArray(amount, valueToFill) {
+  let add = []
+  for (let i = 0; i < amount; i++) {
     add.push(valueToFill)
-}
-return add
+  }
+  return add
 }
 console.log(fillArray(amount, valueToFill))
-
-
 
 
 
@@ -21,86 +19,11 @@ let obj = {
   count: 4
 }
 
-
 function multiplyNumeric() {
-  for (let yyy in obj) {
-    if (typeof obj[yyy] === 'number') { obj[yyy] ** 2 }
-  }
+  for (let yyy in obj){
+  if (typeof obj[yyy] === 'number'){obj[yyy] **= 2}}
 }
 multiplyNumeric(obj)
 console.log(obj)
-
-
-
-
-
-
-
-
-
-
-
-
-// / // Задание №2 (НЕ ПОНИМАЮ ЧТО Я ДЕЛАЮ НЕ ТАК, ПРОГРАММА НЕ РАБОТАЕТ, МОЖНО РЕШЕНИЕ?)
-// let obj = {
-//   width: 10, 
-//   height: 5,
-//   title: 'Test Array',
-//   count: 4
-// }
-
-
-// function multiplyNumeric(){
-//   let values = Object.values(obj)
-//   values.forEach(function(item){
-//     if (typeof item  === 'number'){'number'**2}})
-//   return  values
-// }
-
-//  multiplyNumeric(obj)
-//   console.log(obj)
-
-
-
-// // // Задание №2 (НЕ ПОНИМАЮ ЧТО Я ДЕЛАЮ НЕ ТАК, ПРОГРАММА НЕ РАБОТАЕТ, МОЖНО РЕШЕНИЕ?)
-// let obj = {
-//   width: 10,
-//   height: 5,
-//   title: 'Test Array',
-//   count: 4
-// }
-// function multiplyNumeric() {
-//   let obj_final = {keys, values}
-//   let keys = Object.keys(obj)
-//   for (let key = 0; key < keys.length; key++) {
-//   }
-//   let values = Object.values(obj)
-//   for (let i = 0; i < values.length; i++) {
-//     if (typeof i === 'number') { 'number' ** 2 }
-//   }
-//   return obj_final
-// }
-// multiplyNumeric(obj)
-// console.log(obj)
-
-
-// // Задание №2 (НЕ ПОНИМАЮ ЧТО Я ДЕЛАЮ НЕ ТАК, ПРОГРАММА НЕ РАБОТАЕТ, МОЖНО РЕШЕНИЕ?)
-// let obj = {
-//   width: 10,
-//   height: 5,
-//   title: 'Test Array',
-//   count: 4
-// };
-
-
-// for(let yyy in obj){
-//   if (typeof'number' == obj[yyy]) {obj[yyy] * obj[yyy] }
-// }
-
-// console.log(obj)
-
-
-
-// Задание №2 (НЕ ПОНИМАЮ ЧТО Я ДЕЛАЮ НЕ ТАК, ПРОГРАММА НЕ РАБОТАЕТ, МОЖНО РЕШЕНИЕ?)
 
 
